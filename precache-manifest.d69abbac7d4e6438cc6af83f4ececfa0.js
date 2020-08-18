ea460f435e690d915f2a626f2587d3f49ee15a83
g3kgirl <shikha.relieved@gmail.com>
2020-08-18 17:35:24 +0530

    Updates

---
+++ precache-manifest.d69abbac7d4e6438cc6af83f4ececfa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c0c23fb64c091a501591bbcd63dfd7",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "f9955d67451c66651e98",
+    "url": "/react-to-do/static/css/main.700210e2.chunk.css"
+  },
+  {
+    "revision": "d1b2c479a2f41fbf62af",
+    "url": "/react-to-do/static/js/2.ffa2648b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do/static/js/2.ffa2648b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9955d67451c66651e98",
+    "url": "/react-to-do/static/js/main.911c815a.chunk.js"
+  },
+  {
+    "revision": "34bc805e57a78bde92bb",
+    "url": "/react-to-do/static/js/runtime-main.b92a3415.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-do/static/media/logo.5d5d9eef.svg"
+  }
+]);
